c6b76cf8765f2118c8f2d1754e5b518671196a93
Incredible-Mack <mackemma96@gmail.com>
2023-04-05 00:19:39 +0100

    removed all white space inserting new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,10 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - inserting  new node to the  linked list
- *                           
- * @head : pointer 
- * @idx  : is the index of the list where the new node should be added. Index starts at 0
+ * insert_nodeint_at_index - inserting  new node
+ * @head : pointer
+ * @idx  : is the index of the list
  * @n    : new value
- * t rep temp
- * Return: the address of the new node, or NULL if it failed
- *      
+ * Return: address the new node|NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
