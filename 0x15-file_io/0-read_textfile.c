b8c44975a7e7f24dafba196798a8ef0551bc98af
Incredible-Mack <mackemma96@gmail.com>
2023-04-11 02:53:54 +0100

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints 
+ * read_textfile - reads a text file and prints
  * @filename: file to be read
  * @letters: etters it should read and print
  * Return: actual number of letters it could read and print
